5fae32eba202de35da5cdf096171dc728d90bcf0
Marcel Pernia <marcelpm1987@gmail.com>
2024-03-11 23:37:51 +0100

    Se crea footer, se integra astro-icons, se refactoriza header...

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+		},
 		extend: {
+			fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
 			colors: {
 				primary: '#003865',
 				secondary: '#005eb8',
 				terciary: '#bd9b60',
-				text: '#363636',
 				danger: '#da343c',
 				success: '#00a3e0',
 				warning: '#f4b400',
