4b4346ec3b27afcacadb16f3bc5f863c6dbea9c7
Marcel Pernia <marcelpm1987@gmail.com>
2024-03-11 23:15:17 +0100

    Actualiza favicon y estilos de la página de inicio. Se crea menu de navegacion

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#003865',
+				secondary: '#005eb8',
+				terciary: '#bd9b60',
+				text: '#363636',
+				danger: '#da343c',
+				success: '#00a3e0',
+				warning: '#f4b400',
+			},
+		},
 	},
 	plugins: [require("daisyui")],
 }
