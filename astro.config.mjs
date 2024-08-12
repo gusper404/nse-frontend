8cba08ef5a059c97f8ceb6d05be6ed0dcc83dc47
Marcel Pernia <marcelpm1987@gmail.com>
2024-08-12 09:51:20 +0200

    ajustes astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ export default defineConfig({
   integrations: [tailwind(), icon(), react()],
   image: {
     domains: ["https://img.youtube.com/"],
+  },
+  site: {
+    title: "NSE Internacional.",
+    description: "NSE (Nuestra Señora del Encuentro con Dios) Internacional es un portal de multimedia que promueve el material audiovisual de NSE TV y NSE Radio.",
+    url: "https://nse-frontend-three.vercel.app",
   }
   // output: "hybrid",
 });
